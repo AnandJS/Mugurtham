45b9fa41f90050e21df369f7d0920b490b0f0442
Anand Jayaseelan <anand.elan@gmail.com>
2017-03-04 20:17:18 -0500

    Ecommerce filters SubCaste and Star added in MyMatching Filters

--- Mugurtham.Service/Scripts/Mugurtham.JS/Mugurtham/Controllers/UserChamber/ControllerMyMatchingProfiles.js
+++ Mugurtham.Service/Scripts/Mugurtham.JS/Mugurtham/Controllers/UserChamber/ControllerMyMatchingProfiles.js
@@ -24,28 +24,58 @@ var ControllerMyMatchingProfiles = angular.module('MugurthamApp').
                                                  $scope.pageSize = 15;
                                                  setTimeout(displayThumbnailSlider, 10)
 
-                                                 $scope.getHighlightedProfiles = function () {
+                                                 $scope.getMyMatchingProfiles = function () {
                                                      setTimeout(displayThumbnailSlider, 10)
 
                                                      FactoryMatchingProfiles.getMatchingProfiles();
+
+                                                   
+
                                                      $("#divContainer").unmask();
                                                      $scope.pageHeader = 'LYTPROFILESMYMATCHES';
                                                      $scope.currentPage = 1;
                                                      $scope.pageSize = 15;
-                                                     
+
                                                      $scope.AllProfiles = FactoryMatchingProfiles.AllProfiles;
                                                      $scope.SearchedProfiles = FactoryMatchingProfiles.SearchedProfiles;
                                                      $scope.profilePhotos = FactoryMatchingProfiles.profilePhotos;
 
                                                      $scope.pageChangeHandler = function (num) {
                                                          $("html, body").animate({ scrollTop: 220 }, "slow");
-                                                         setTimeout(displayThumbnailSlider, 100);
+                                                         setTimeout(displayThumbnailSlider, 10);
                                                      };
                                                      $scope.pageChangeHandlerSmartSearch = function (num) {
-                                                         setTimeout(displayThumbnailSlider, 100);
+                                                         setTimeout(displayThumbnailSlider, 10);
                                                      };
                                                      toastr.success('My Matching Profiles loaded Successfully');
                                                  }
+
+                                                 /*========================================= E-Commerce Filter Section ======================================================*/
+                                                 $scope.getStarFilterItemCount = function (star) {
+                                                     return FactoryMatchingProfiles.getStarFilterItemCount(star, $scope.SearchedProfiles);
+                                                 }
+
+                                                 $scope.getSubCasteFilterItemCount = function (subCaste) {
+                                                     return FactoryMatchingProfiles.getSubCasteFilterItemCount(subCaste, $scope.SearchedProfiles);
+                                                 };
+                                                 $scope.filterStarByThisItem = function (data) {
+                                                     FactoryMatchingProfiles.filterStarByThisItem(data);
+                                                 };
+                                                 $scope.filterSubCasteByThisItem = function (data) {
+                                                     FactoryMatchingProfiles.filterSubCasteByThisItem(data);
+                                                 };
+                                                 $scope.colourFilter = function (data) {
+                                                    return FactoryMatchingProfiles.colourFilter(data);
+                                                 };
+                                                 $scope.subcasteFilter = function (data) {
+                                                     return FactoryMatchingProfiles.subcasteFilter(data);
+                                                 };
+                                                 
+                                                 
+                                                 /*========================================= E-Commerce Filter Section End======================================================*/
+
+
+
                                                  // Wiring up with Template through Custom Directive
                                                  $scope.objScope = $scope;
                                              }])
--- Mugurtham.Service/Scripts/Mugurtham.JS/Mugurtham/Factories/FactoryMatchingProfile.js
+++ Mugurtham.Service/Scripts/Mugurtham.JS/Mugurtham/Factories/FactoryMatchingProfile.js
@@ -63,11 +63,11 @@ app.factory('FactoryMatchingProfiles', ['$http', 'ConstantMatchingStarsForGroom'
         });
     }
 
-    function initData($http, data) {        
+    function initData($http, data) {
         var _star = '';
         if (typeof (Storage) !== "undefined") {
             _star = JSON.parse(localStorage.getItem("LoggedInUser")).BasicInfoCoreEntity.Star;
-        }        
+        }
         serviceFactoryMatchingStar.AllProfiles = data;
         serviceFactoryMatchingStar.SearchedProfiles = getMatchingProfiles(data, _star);
         serviceFactoryMatchingStar.profilePhotos = data.PhotoCoreEntityList;
@@ -93,6 +93,83 @@ app.factory('FactoryMatchingProfiles', ['$http', 'ConstantMatchingStarsForGroom'
         });
         return matchingProfiles;
     }
+
+
+    /*========================================= E-Commerce Filter Section ======================================================*/
+    serviceFactoryMatchingStar.getStarFilterItemCount = function (_star, SearchedProfiles) {
+        var counter = 0;
+        angular.forEach(SearchedProfiles, function (value, key) {
+            if (value.Star == _star)
+                counter = parseInt(counter + 1);
+        });
+        return parseInt(counter);
+    };
+
+    serviceFactoryMatchingStar.getSubCasteFilterItemCount = function (subCaste, SearchedProfiles) {
+        var counter = 0;
+        angular.forEach(SearchedProfiles, function (value, key) {
+            if (value.SubCaste == subCaste)
+                counter = parseInt(counter + 1);
+        });
+        return parseInt(counter);
+    };
+
+    serviceFactoryMatchingStar.filterItem = [];
+    serviceFactoryMatchingStar.filterSubCasteItem = [];
+
+
+    serviceFactoryMatchingStar.filterStarByThisItem = function (data) {
+         var i = $.inArray(data, serviceFactoryMatchingStar.filterItem);
+        if (i > -1) {
+            serviceFactoryMatchingStar.filterItem.splice(i, 1);
+        } else {
+            serviceFactoryMatchingStar.filterItem.push(data);
+        }
+        setTimeout(displayThumbnailSlider, 10);
+        $("html, body").animate({ scrollTop: 220 }, "slow"); 
+    };
+
+    serviceFactoryMatchingStar.filterSubCasteByThisItem = function (data) {
+        var i = $.inArray(data, serviceFactoryMatchingStar.filterSubCasteItem);
+        if (i > -1) {
+            serviceFactoryMatchingStar.filterSubCasteItem.splice(i, 1);
+        } else {
+            serviceFactoryMatchingStar.filterSubCasteItem.push(data);
+        }
+        setTimeout(displayThumbnailSlider, 10);
+        $("html, body").animate({ scrollTop: 220 }, "slow");
+    };
+
+     
+    serviceFactoryMatchingStar.colourFilter = function (item) {
+        if (serviceFactoryMatchingStar.filterItem.length > 0) {
+            if ($.inArray(item.Star, serviceFactoryMatchingStar.filterItem) < 0)
+                return;
+        }
+        return item.Star;
+    }
+    serviceFactoryMatchingStar.starFilter = function (item) {
+        if (serviceFactoryMatchingStar.filterItem.length > 0) {
+            if ($.inArray(item.Star, serviceFactoryMatchingStar.filterItem) < 0)
+                return;
+        }
+        return item.Star;
+    }
+    serviceFactoryMatchingStar.subcasteFilter = function (item) {
+        if (serviceFactoryMatchingStar.filterSubCasteItem.length > 0) {
+            if ($.inArray(item.SubCaste, serviceFactoryMatchingStar.filterSubCasteItem) < 0)
+                return;
+        }
+        return item.SubCaste;
+    }
+
+
+
+
+
+
+    /*========================================= E-Commerce Filter Section End======================================================*/
+
 
     return serviceFactoryMatchingStar;
 }]);
